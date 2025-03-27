3f8760d611d9850cccf44f934eeece56ca4b60ce
danaguil <danaguilarfabian@gmail.com>
2025-03-27 12:50:14 -0700

    This code will be used to create a login page for Underground Threads, used with terminal. Still needs to be implemented with a GUI

---
+++ loginFunctionality/src/userClass.java
@@ -0,0 +1,34 @@
+/*
+    User class, will be containing
+        - email, username, and password. All which the user can change if needed
+    We have the function getEmail, getUsername etc.. in order to check for valid credentials when logining in
+
+    We could've used a record java class, only one line of code for maximum efficiency, BUT
+     we won't be able to change user information when needed.
+ */
+public class User {
+    // User values
+    private String email;
+    private String username;
+    private String password;  // Added password field
+
+    // Initializing when creating a new user
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Helper functions to check for valid login information
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
