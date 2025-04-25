3745fa52e936c5e360f9b704d536cdd539a5b23e
pauloperez <w.pauloaperez@gmail.com>
2025-04-25 14:31:53 -0700

    Refactored Brand Side UI classes. Added functionality to save uploaded images to the program package "images"

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,14 @@
 import UGT_UI.*;
 import javax.swing.*;
-import UGT_Controllers.LoginController;
+
 import java.io.FileNotFoundException;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws FileNotFoundException{
+
         // new Buyer();
-        // new BrandProfile();
-
-        try {
-            LoginController.populateHashMap();
-            new Login();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: userInfoFile.txt not found!");
-            throw new FileNotFoundException();
-        }
+        //new ProfilePage();
+        new UploadPage(0, 0, 0, 0);
 
 
     }
